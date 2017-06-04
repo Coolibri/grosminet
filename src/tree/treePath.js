b9cad375308dfcee5c1dd6341e4b38209ada1f23
Paul Jarleton <sarlam@minet.net>
2017-06-04 18:28:39 +0200

    split turns into pieces ha ha ha ha ha

--- src/tree/treePath.js
+++ src/tree/treePath.js
@@ -1,3 +1,5 @@
+import childs from './turns/turn01'
+
 export default {
   text: 'Bienvenu sur votre planète, pb de surpopulation et donc de famine, que faire ?',
   choices: [
@@ -9,6 +11,12 @@ export default {
         } else {
           return 'well, pour ' + nbP + ' pays ce sera OGM ! '
         }
+      },
+      points: {
+        food: {value: 4, turns: [1, 0.5, 0, -0.5]},
+        water: {value: 0, turns: [0, 0, 0, 0]},
+        waste: {value: 0, turns: [0, 0, 0, 0]},
+        energy: {value: -2, turns: [1, 0, 0, 0]}
       }
     },
     {
@@ -18,6 +26,12 @@ export default {
           return 'personne n\'a choisi les engrais comme solutions '
         }
         return 'arosons les plentations de ' + nbP + ' pays d\'engrais. '
+      },
+      points: {
+        food: {value: 4, turns: [1, 1, 0.75, 0.5]},
+        water: {value: -5, turns: [0.5, 1, 0.25, 0]},
+        waste: {value: -2, turns: [0, 0, 0.5, 1]},
+        energy: {value: 0, turns: [1, 0, 0, 0]}
       }
     },
     {
@@ -27,72 +41,14 @@ export default {
           return 'personne ne peux test !'
         }
         return 'ceci est un test selon ' + nbP + ' de joueurs. '
+      },
+      points: {
+        food: {value: 4, turns: [1, 0.5, 0, -0.5]},
+        water: {value: 0, turns: [0, 0, 0, 0]},
+        waste: {value: 0, turns: [0, 0, 0, 0]},
+        energy: {value: -2, turns: [1, 0, 0, 0]}
       }
     }
   ],
-  childs: [
-    {
-      text: 'most of you chose OGM, so let\'s see OGM results on planet !',
-      choices: [
-        {
-          label: 'Beer',
-          value: (pName) => {
-            return pName + ' make a great choice ! '
-          }
-        },
-        {
-          label: 'Water',
-          value: (pName) => {
-            return 'pour une bonne santée, ' + pName + 'choisis l\'eau et c\'est raisonnable'
-          }
-        },
-        {
-          label: 'soda',
-          value: (pName) => { return pName + ' veux du sucre,plus de sucre ! ' }
-        }
-      ]
-    },
-    {
-      text: 'most of you chose engrais, so let\'s see engrais results on planet !',
-      choices: [
-        {
-          label: 'Beer',
-          value: (pName) => {
-            return pName + ' make a great choice ! '
-          }
-        },
-        {
-          label: 'Water',
-          value: (pName) => {
-            return 'pour une bonne santée, ' + pName + 'choisis l\'eau et c\'est raisonnable'
-          }
-        },
-        {
-          label: 'soda',
-          value: (pName) => { return pName + ' veux du sucre,plus de sucre ! ' }
-        }
-      ]
-    },
-    {
-      text: 'most of you chose engrais, so let\'s see engrais results on planet !',
-      choices: [
-        {
-          label: 'Beer',
-          value: (pName) => {
-            return pName + ' make a great choice ! '
-          }
-        },
-        {
-          label: 'Water',
-          value: (pName) => {
-            return 'pour une bonne santée, ' + pName + 'choisis l\'eau et c\'est raisonnable'
-          }
-        },
-        {
-          label: 'soda',
-          value: (pName) => { return pName + ' veux du sucre,plus de sucre ! ' }
-        }
-      ]
-    }
-  ]
+  childs: childs
 }
---
+++ src/tree/turns/turn01.js
@@ -0,0 +1,65 @@
+export default [
+  {
+    text: 'most of you chose OGM, so let\'s see OGM results on planet !',
+    choices: [
+      {
+        label: 'Beer',
+        value: (pName) => {
+          return pName + ' make a great choice ! '
+        }
+      },
+      {
+        label: 'Water',
+        value: (pName) => {
+          return 'pour une bonne santée, ' + pName + 'choisis l\'eau et c\'est raisonnable'
+        }
+      },
+      {
+        label: 'soda',
+        value: (pName) => { return pName + ' veux du sucre,plus de sucre ! ' }
+      }
+    ]
+  },
+  {
+    text: 'most of you chose engrais, so let\'s see engrais results on planet !',
+    choices: [
+      {
+        label: 'Beer',
+        value: (pName) => {
+          return pName + ' make a great choice ! '
+        }
+      },
+      {
+        label: 'Water',
+        value: (pName) => {
+          return 'pour une bonne santée, ' + pName + 'choisis l\'eau et c\'est raisonnable'
+        }
+      },
+      {
+        label: 'soda',
+        value: (pName) => { return pName + ' veux du sucre,plus de sucre ! ' }
+      }
+    ]
+  },
+  {
+    text: 'most of you chose engrais, so let\'s see engrais results on planet !',
+    choices: [
+      {
+        label: 'Beer',
+        value: (pName) => {
+          return pName + ' make a great choice ! '
+        }
+      },
+      {
+        label: 'Water',
+        value: (pName) => {
+          return 'pour une bonne santée, ' + pName + 'choisis l\'eau et c\'est raisonnable'
+        }
+      },
+      {
+        label: 'soda',
+        value: (pName) => { return pName + ' veux du sucre,plus de sucre ! ' }
+      }
+    ]
+  }
+]
