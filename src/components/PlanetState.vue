6a3ede0f6b77eaebed34b597e28d2d1b397fcfd2
Paul Jarleton <sarlam@minet.net>
2017-06-05 12:01:59 +0200

    lot's of data !

--- src/components/PlanetState.vue
+++ src/components/PlanetState.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <state :state="state">
+  <div id="global-state">
+    <state :global="true" :state="state">
     </state>
-    {{ global }}
+    <div class="global-range"><label>global</label><input disabled type="range" :value="global"/></div>
   </div>
 </template>
 
@@ -13,8 +13,29 @@
     name: 'planet-state',
     props: ['state', 'global'],
     data: function () {
-      return {
-      }
+      return {}
     }
   }
 </script>
+
+<style scoped>
+  #global-state{
+    display: block;
+    width:100%;
+  }
+  .global-range {
+    width: 100%;
+    display: block;
+  }
+
+
+  div > label {
+    display:inline-block;
+    width: 30%;
+  }
+
+  div > input {
+    display: inline-block;
+    width: 70%;
+  }
+</style>
--- src/components/State.vue
+++ src/components/State.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="sensors">
-    {{ state.water }}, {{ state.waste }}, {{ state.energy }}, {{ state.food }}
+  <div class="sensors" :class="{ global: global }">
+    <div>
+      <label>Water</label><input type="range" :value="state.water" disabled/>
+    </div>
+    <div>
+      <label>Waste</label><input type="range" :value="state.waste" disabled/>
+    </div>
+    <div>
+      <label>Energy</label><input type="range" :value="state.energy" disabled/>
+    </div>
+    <div>
+      <label>Food</label><input type="range" :value="state.food" disabled/>
+    </div>
   </div>
 </template>
 
@@ -8,11 +19,40 @@
   export default{
     name: 'state',
     props: [
-      'state'
+      'state',
+      'global'
     ]
   }
 </script>
 
-<style>
+<style scoped>
+  .sensors {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-items: flex-start;
+    height: 120px;
+  }
 
+  div.global {
+    height: 120px;
+  }
+
+  .sensors div {
+    box-sizing: border-box;
+    flex: 1;
+    width: 50%;
+    height: 60px;
+    display: block; /* IE fix */
+  }
+
+  .sensors div label {
+    width: 20%;
+    display: inline-block;
+  }
+
+  .sensors div input {
+    width: 80%;
+    display: inline-block;
+  }
 </style>
